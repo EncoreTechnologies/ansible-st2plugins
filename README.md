a9344e185a6da90f668ebb85152d3b303a303f53
Nick Maludy <nmaludy@gmail.com>
2018-01-19 22:12:34 -0500

    Initial implementation of the st2 lookup plugin for Ansible

--- README.md
+++ README.md
@@ -1,2 +1,198 @@
 # ansible-st2plugins
+
 Plugins for ansible that communicate with the StackStorm (st2) API.
+
+## Installation
+
+### Pre-requisites 
+
+This plugin uses the `requests` python package (found [here](http://docs.python-requests.org/en/master/user/install/)).
+You'll need to install that package so that it's available to ansible during runtime.
+
+``` shell
+pip install requests
+```
+
+### Option 1 - Standard Directories
+
+Copy the `lookup_plugins` directory into one of the following default directories
+secified here: https://docs.ansible.com/ansible/devel/config.html#default-lookup-plugin-path
+
+Default Directories:
+```shell
+~/.ansible/plugins/lookup_plugins/
+/usr/share/ansible_plugins/lookup_plugins
+```
+
+### Option 2 - Playbook Directory
+
+According to the [lookup_plugin documentation](https://docs.ansible.com/ansible/devel/plugins/lookup.html#enabling-lookup-plugins)
+you can also copy the `lookup_plugins` directory into the same directory as your
+playbook:
+
+```shell
+cp -r lookup_plugins/ /path/to/directory/where/your/playbook/lives/
+```
+
+### Option 3 - Ansible Config
+
+Edit your ansible config `ansible.cfg` and add in the path to this repositories 
+`lookup_plugins` directory into the `[defaults]` section:
+
+``` ini
+[defaults]
+lookup_plugins = ~/.ansible/plugins/lookup_plugins/:/usr/share/ansible_plugins/lookup_plugins:~/git/ansible-st2plugins/lookup_plugins
+```
+
+## Usage
+
+### Lookup Plugins
+
+The `st2` plugin reads data from the StackStorm (st2) key/value datastore
+from within ansible. 
+
+``` yaml
+  - name: retrieving a KV from StackSTorm
+    debug:
+      msg: "{{ lookup('st2', 'system.my_key') }}"
+```
+
+In order for this to work ansible will need to authenticate with StackStorm.
+Authentication can be accomplished in several ways:
+
+ * API key from within ansible
+ * API key from an environment variable
+    * `ST2_API_KEY `
+ * Auth token from within ansible
+ * Auth token from an environment variable(s)
+    * `ST2_ACTION_AUTH_TOKEN`
+    * `ST2_AUTH_TOKEN`
+
+#### API Key Auth
+
+In this first example we're retrieving the key `system.my_key` from a remote
+StackStorm host using an API key generated from command `st2 apikey create`.
+For more info on creating API keys see the StackStorm documentation [here](https://docs.stackstorm.com/authentication.html#api-keys).
+Once the API key is obtained you can simply pass it in to the `lookup` function
+using the parameter `api_key`:
+
+``` yaml
+  - name: retrieving a KV from a remote host using an API key
+    debug:
+      msg: "{{ lookup('st2', 'system.my_key', hostname='stackstorm.domain.tld', api_key="xyz123") }}"
+```
+
+Sometimes it's conveient to utilize environment variables to store information, 
+for example when invoking multiple consecutive playbooks. In this case the plugin
+allows the API key to be read from an environment variable `ST2_API_KEY`. When
+using an environment variable, make sure to NOT specify the `api_key` parameter
+in the lookup function:
+
+
+``` yaml
+  - name: retrieving a KV from a remote host using an API key (assumes ST2_API_KEY 
+        environment variable is set)
+    debug:
+      msg: "{{ lookup('st2', 'system.my_key', hostname='stackstorm.domain.tld') }}"
+```
+
+#### Auth Token
+
+Auth tokens work the same as API keys except different names are used.
+For an auth token passed directly into the lookup function use the `auth_token` 
+paramter:
+
+``` yaml
+  - name: retrieving a KV from a remote host using an Auth token
+    debug:
+      msg: "{{ lookup('st2', 'system.my_key', hostname='stackstorm.domain.tld', auth_token="ysfd456") }}"
+```
+
+For utilizing environment variables you can either set `ST2_AUTH_TOKEN` or 
+`ST2_ACTION_AUTH_TOKEN`. 
+
+#### Calling from a StackStorm action
+
+We've tried to make the ansible integration as easy as possible. If you
+invoke ansible from a StackStorm action using something like the 
+[`stackstorm-ansible`](https://github.com/StackStorm-Exchange/stackstorm-ansible)
+or the [`local-shell-cmd` runner](https://docs.stackstorm.com/reference/runners.html#local-command-runner-local-shell-cmd)
+then the authentication is handled for you automatically! This happens
+because when StackStorm invokes an action it sets the environment
+variable `ST2_ACTION_AUTH_TOKEN` with the auth token of the current action
+being run. This means, when calling directly from StackStorm, can your
+lookup can be as simple as:
+
+``` yaml
+  - name: retrieving a KV from StackSTorm (when called from StackStorm)
+    debug:
+      msg: "{{ lookup('st2', 'system.my_key') }}"
+```
+
+#### Reference
+
+A complete reference documentation for the `st2` lookup plugin:
+
+```yaml
+lookup: st2
+version_added: N/A
+short_description: Grab values from the StackStorm (st2) key/value datastore
+description:
+  - lookup metadata for a playbook from the key value store in StackStorm (st2).
+requirements:
+  - 'python-requests (python library http://docs.python-requests.org/en/master/user/install/)'
+options:
+  _terms:
+    description: key(s) to retrieve
+    type: list
+    required: True
+  api_url:
+    type: string
+    description: URL of the API endpoint on the StackStorm server.
+    default: https://localhost/api
+    required: False
+  hostname:
+    type: string
+    description:
+      - If api_url isn't specified then this will be the hostname used
+        to create the URL string: https://<hostname>/api
+    default: localhost
+    required: False
+  port:
+    type: string
+    description:
+      - If api_url isn't specified then this will be the port used
+        to create the URL string: https://<hostname>:<port>/api
+      - If no port is specified (default) then the :<port> will not be used
+        when creating the URL string.
+    default: None
+    required: False
+  ssl_verify:
+    type: boolean
+    description: Should verification be performed for the SSL certificate?
+    default: true
+  auth_token:
+    type: string
+    description: Authentication token received from StackStorm auth.
+    env:
+      - name: ST2_ACTION_AUTH_TOKEN
+      - name: ST2_AUTH_TOKEN
+  api_key:
+    type: string
+    description:
+       - API key generated in StackStorm. Can be created using the
+         command: 'st2 apikey create'
+    env:
+      - name: ST2_API_KEY
+  decrypt:
+    type: boolean
+    description:
+      - Should the value be decrypted by StackStorm. This should be set to
+        True if you would like to read encrypted keys.
+    default: False
+  user:
+    type: string
+    description:
+      - Username of the StackStorm user that owns the key.
+      - Needed when reading user-scoped keys
+```
---
+++ lookup_plugins/st2.py
@@ -0,0 +1,245 @@
+# (c) 2018, Nick Maludy <nmaludy@gmail.com>
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = """
+    lookup: st2
+    version_added: N/A
+    short_description: Grab values from the StackStorm (st2) key/value datastore
+    description:
+      - lookup metadata for a playbook from the key value store in StackStorm (st2).
+    requirements:
+      - 'python-requests (python library http://docs.python-requests.org/en/master/user/install/)'
+    options:
+      _terms:
+        description: key(s) to retrieve
+        type: list
+        required: True
+      api_url:
+        type: string
+        description: URL of the API endpoint on the StackStorm server.
+        default: https://localhost/api
+        required: False
+      hostname:
+        type: string
+        description:
+          - If api_url isn't specified then this will be the hostname used
+            to create the URL string: https://<hostname>/api
+        default: localhost
+        required: False
+      port:
+        type: string
+        description:
+          - If api_url isn't specified then this will be the port used
+            to create the URL string: https://<hostname>:<port>/api
+          - If no port is specified (default) then the :<port> will not be used
+            when creating the URL string.
+        default: None
+        required: False
+      ssl_verify:
+        type: boolean
+        description: Should verification be performed for the SSL certificate?
+        default: true
+      auth_token:
+        type: string
+        description: Authentication token received from StackStorm auth.
+        env:
+          - name: ST2_ACTION_AUTH_TOKEN
+          - name: ST2_AUTH_TOKEN
+      api_key:
+        type: string
+        description:
+           - API key generated in StackStorm. Can be created using the
+             command: 'st2 apikey create'
+        env:
+          - name: ST2_API_KEY
+      decrypt:
+        type: boolean
+        description:
+          - Should the value be decrypted by StackStorm. This should be set to
+            True if you would like to read encrypted keys.
+        default: False
+      user:
+        type: string
+        description:
+          - Username of the StackStorm user that owns the key.
+          - Needed when reading user-scoped keys
+"""
+
+EXAMPLES = """
+  - debug: msg='key contains {{item}}'
+    with_st2:
+      - 'system.key_to_retrieve'
+      - 'st2kv.system.key_to_retrieve'
+
+  - name: retrieving a KV when ansible is called from a StackStorm action
+    debug:
+      msg: "{{ lookup('st2', 'system.some_key') }}"
+
+  - name: retrieving a KV from a remote host using an API key
+    debug:
+      msg: "{{ lookup('st2', 'system.my_key', hostname='stackstorm.domain.tld', api_key="xyz123") }}"
+
+  - name: retrieving a KV from a remote host using an Auth token
+    debug:
+      msg: "{{ lookup('st2', 'system.my_key', hostname='stackstorm.domain.tld', auth_token="ysfd456") }}"
+
+  - name: retrieving a KV in user scope
+    debug:
+      msg: "{{ lookup('st2', 'user.my_key', user='dave', hostname='stackstorm.domain.tld', api_key="xyz123") }}"
+
+  - name: retrieving a KV using a custom API url
+    debug:
+      msg: "{{ lookup('st2', 'system.different_key', api_url='http://st2.domain.tld/st2/api, api_key="xyz123") }}"
+"""
+
+RETURN = """
+  _term:
+    description:
+      - value(s) stored in the StackStorm (st2) datastore
+"""
+
+import os
+import sys
+from ansible.module_utils.six.moves.urllib.parse import urlparse
+from ansible.errors import AnsibleError
+from ansible.plugins.lookup import LookupBase
+
+try:
+    import requests
+    HAS_REQUESTS = True
+except ImportError as e:
+    HAS_REQUESTS = False
+
+
+class LookupModule(LookupBase):
+
+    def run(self, terms, variables=None, **kwargs):
+        if not HAS_REQUESTS:
+            raise AnsibleError('"requests" module is required for st2_kv lookup. see http://docs.python-requests.org/en/master/user/install/')
+
+        values = []
+        try:
+            for term in terms:
+                api_url = kwargs.get('api_url', None)
+                if not api_url:
+                    hostname = kwargs.get('hostname', 'localhost')
+                    port = kwargs.get('port', None)
+                    if port:
+                        api_url = 'https://{}:{}/api'.format(hostname, port)
+                    else:
+                        api_url = 'https://{}/api'.format(hostname)
+
+                ssl_verify = kwargs.get('ssl_verify', True)
+                if not ssl_verify:
+                    requests.packages.urllib3.disable_warnings()
+
+                # Priority:
+                #  1. auth_token : argument to the function
+                #  2. ST2_ACTION_AUTH_TOKEN : environment variable
+                #  3. ST2_AUTH_TOKEN : environment variable
+                auth_token = kwargs.get('auth_token', None)
+                if not auth_token:
+                    auth_token = os.environ.get('ST2_ACTION_AUTH_TOKEN')
+                if not auth_token:
+                    auth_token = os.environ.get('ST2_AUTH_TOKEN')
+
+                # Priority:
+                #  1. api_key : argument to the function
+                #  2. ST2_API_KEY : environment variable
+                api_key = kwargs.get('api_key', None)
+                if not api_key:
+                    api_key = os.environ.get('ST2_API_KEY')
+
+                decrypt = kwargs.get('decrypt', False)
+                user = kwargs.get('user', None)
+
+                headers = {}
+                if auth_token:
+                    headers['X-Auth-Token'] = auth_token
+                elif api_key:
+                    headers['St2-Api-Key'] = api_key
+                else:
+                    raise AnsibleError(
+                        "Error no auth information provided. You must specify"
+                        " either auth_token or api_key parameters. Or, set one"
+                        " of the following environment variables: ST2_AUTH_TOKEN,"
+                        " ST2_ACTION_AUTH_TOKEN, or ST2_API_KEY")
+
+                # parse the term into scope and key
+                scope, key = self.parse_scope_key(term)
+
+                # build the URL based on scope and key names
+                url = '{}/v1/keys/{}?scope={}'.format(api_url, key, scope)
+                # append the global options to the URL
+                if decrypt:
+                    url += "&decrypt=true"
+                if user:
+                    url += "&user={}".format(user)
+
+                # make the query
+                response = requests.get(url, headers=headers, verify=ssl_verify)
+                response.raise_for_status()
+                data = response.json()
+
+                # append to results
+                values.append(data["value"])
+        except Exception as e:
+            raise AnsibleError(
+                "Error locating '%s' in kv store. Error was %s" % (term, e))
+
+        return values
+
+    def parse_scope_key(self, term):
+        """Parses a term string into a scope and key.
+        The valid formats for the string is:
+          key        ('system' scope assumed)
+          scope.key
+          st2kv.scope.key
+
+        The parts of the string are delimited by '.'.
+        If a string is given with no '.' then the given string will be the key
+        and the 'system' scope will be used as the default.
+        If the first part of the string is NOT "st2kv" the the first part will
+        be used as the scope and the remaining parts will be joined back together
+        with '.' and used as the key.
+        If the first part of the string is "st2kv" then this part will be removed
+        and the rest of the string will be parsed like a 2-part string (above).
+
+        Examples:
+          term: my_really_sweet_key
+            scope: system
+            key: my_really_sweet_key
+
+          term: system.my.coolkey
+            scope: system
+            key: my.coolkey
+
+          term: st2kv.user.some.fancy_key_name.fun
+            scope: user
+            key: some.fancy_key_name.fun
+        """
+        # is the term None or empty string?
+        if not term:
+            raise AnsibleError("Error - The key can't be an empty string or null")
+
+        # break apart the term into parts using '.' as the delimiter
+        parts = term.split('.')
+        if len(parts) == 1:
+            return ("system", parts[0])
+
+        # remove "st2kv" if it's the first part
+        if parts[0] == "st2kv":
+            parts.pop(0)
+
+        # does the term contain the required 2 parts (scope and key)?
+        if len(parts) < 2:
+            raise AnsibleError("Error - The key doesn't contain a scope and a"
+                               " key delimited by a '.' (example: system.key)."
+                               " Error in key: {}".format(term))
+
+        # the scope is the text before the first '.'
+        scope = parts[0]
+        # the key is the text after the first '.'
+        key = '.'.join(parts[1:])
+        return (scope, key)
